b6011a4300446042201fd25053c1d0d63d8a8031
ahmedHelmiALi <ahmed.elmagdy51@gmail.com>
2023-10-16 15:02:52 +0200

    movies-component-and-movie-component-added

---
+++ src/app/movies/movies/movies.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { RequestService } from 'src/app/shared/services/request.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css'],
+})
+export class moviesComponent implements OnInit {
+  movies!: any;
+  img_path: string = '';
+  constructor(private requestService: RequestService) {}
+  ngOnInit() {
+    this.requestService.getMovies().subscribe((data: any) => {
+      this.movies = data.results;
+      this.img_path = this.requestService.img_path;
+    });
+  }
+}
