17af6c33eb557974f6454e4f39e0d6e03a9b725f
ahmedHelmiALi <ahmed.elmagdy51@gmail.com>
2023-10-16 12:11:55 +0200

    http-service added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 import { RouterModule } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [NavComponent],
-  imports: [CommonModule, RouterModule, AppRoutingModule],
-  exports: [  NavComponent],
+  imports: [CommonModule, RouterModule, AppRoutingModule, HttpClientModule],
+  exports: [NavComponent, HttpClientModule],
 })
-export class SharedModule {
-}
+export class SharedModule {}
